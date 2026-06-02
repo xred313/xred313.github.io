0ee3acd8c3b2ec5774a9d040a249afd22ebbbff2
xred313 <jakand070512@student.jenseneducation.se>
2026-06-02 12:28:58 +0200

    klar officiel

--- init.js
+++ init.js
@@ -9,7 +9,7 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="Index.html">
+          <div class="collapse navbar-collapse" id="NavbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="Index.html">Home</a>
